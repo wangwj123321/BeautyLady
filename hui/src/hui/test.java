11e71ef5ee34b251edf789171bd75e1837070331
hui <wangwj123321@tom.com>
2017-11-30 16:15:10 +0800

    test

---
+++ hui/src/hui/test.java
@@ -0,0 +1,9 @@
+package hui;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("test");
+		System.out.println("asdfasd");
+		System.out.println("adfasdf");
+	}
+}
